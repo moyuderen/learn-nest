34c42af3437d56acc20545f098ea3a9e6664168c
moyuderen <moyuderen>
2024-04-25 14:40:16 +0800

    provider

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
+
+  @Inject() // Inject的方式注入
+  private readonly appService: AppService;
+
+  @Inject()
+  private readonly person1: { name: string; age: number };
+
+  @Inject()
+  private readonly person2: { name: string; age: number };
+
+  @Inject()
+  private readonly person3: { name: string; desc: string };
+
+  @Inject()
+  private readonly person4: { name: string; age: number };
+
+  @Inject()
+  private readonly person5: { name: string; age: number };
 
   @Get()
   getHello(): string {
+    console.log(this.person1);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,58 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // useClass
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    // 使用固定值
+    {
+      provide: 'person1',
+      useValue: {
+        name: '星光榴莲鸡',
+        age: 18,
+      },
+    },
+    // 函数工厂模式
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: '星光榴莲鸡',
+          age: 18,
+        };
+      },
+    },
+    // 函数工厂模式 可传参数
+    {
+      provide: 'person3',
+      useFactory(person1, appService) {
+        return {
+          name: person1.name,
+          desc: appService.getHello(),
+        };
+      },
+    },
+    // 函数工厂模式 支持异步
+    {
+      provide: 'person4',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 2000);
+        });
+        return {
+          name: '星光榴莲鸡',
+          age: 18,
+        };
+      },
+    },
+    // 别名
+    {
+      provide: 'person5',
+      useExisting: 'person1',
+    },
+  ],
 })
 export class AppModule {}
