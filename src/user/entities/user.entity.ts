4a12f688f399642e0843e5ac5ad21d9023571d02
moyuderen <moyuderen>
2024-05-08 15:42:52 +0800

    feat: typeorm基础

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'user',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+  })
+  name: string;
+}
